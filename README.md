1193821060e3082bedac10e79db15cf05b487950
Ajinkya <ajinkyatambade143@gmail.com>
2024-04-03 23:01:07 +0530

    Project Added

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# DisTalk-ChatApp
+# DisTalk Chat App
+
+## Aim
+The aim of the project is to create a  realtime Chat Application where multiple users can have chat with each other. It is made up of two applications - the client application, which runs on the user
+’s web browser and server application, runs on any hosting servers on the network.In addition, it shows the number of users online and the number of people joining or leaving the chat.
+ 
+ ## Languages and Frameworks Used
+```
+- HTML5
+- CSS3
+- Javascript
+- ExpressJS
+- Socket.io 
+```
+
+## Screenshots of the Website 
+
+### 1. Desktop view 
+
+<img width="970" alt="Capture2" src="https://user-images.githubusercontent.com/60184336/209706377-e8058466-3cae-4da3-80e0-034578dbac71.PNG">
+
+### 2. Mobile view 
+
+![pic-3](https://user-images.githubusercontent.com/60184336/192136838-812fe65f-dde4-4c60-8f99-2389c177d2c9.jpg)
+
+
+
+
+ 
---
+++ server.js
@@ -0,0 +1,38 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+const port = process.env.PORT || 80;
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// Socket.io Set-up
+
+var users = {};
+
+io.on("connection", (socket) => {
+  socket.on("new-user-joined", (username) => {
+    users[socket.id] = username;
+    socket.broadcast.emit("user-connected", username);
+    io.emit("user-list", users);
+  });
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("user-disconnected", (user = users[socket.id]));
+    delete users[socket.id];
+    io.emit("user-list", users);
+  });
+
+  socket.on("message", (data) => {
+    socket.broadcast.emit("message", { user: data.user, msg: data.msg });
+  });
+});
+
+// Socket.io Set-up ends
+server.listen(port, () => {
+  console.log("Server started @ Port : " + port);
+});
